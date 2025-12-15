2598bbca1bd37f52c916cb1a9c86d47461350bf5
zhiyad96 <mhdzhiyad9656@gmail.com>
2025-12-15 15:25:30 +0530

    Time: 61 ms (13.90%) | Memory: 54.3 MB (72.41%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        return Promise.race([
+            fn(...args),
+          new Promise((_,rejected)=>setTimeout(()=> rejected("Time Limit Exceeded"),t)
+            )
+        ])
+       
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
